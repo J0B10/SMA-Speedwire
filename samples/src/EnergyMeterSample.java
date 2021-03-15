ed4996b08ba9e4d4f62189dae9eabcf118fc9f88
Jonas <joblo.2213@gmail.com>
2021-03-15 10:29:16 +0100

    Improve sample

--- samples/src/EnergyMeterSample.java
+++ samples/src/EnergyMeterSample.java
@@ -4,27 +4,34 @@ import de.ungefroren.sma.speedwire.protocol.telegrams.EnergyMeterTelegram;
 import tech.units.indriya.unit.Units;
 
 import javax.measure.MetricPrefix;
+import javax.measure.Quantity;
 import javax.measure.quantity.Energy;
+import javax.measure.quantity.Power;
 import java.io.IOException;
 
 public class EnergyMeterSample {
     public static void main(String[] args) throws IOException {
         Speedwire speedwire = new Speedwire();
         speedwire.onError(Exception::printStackTrace);
-        speedwire.onTimeout(() -> System.out.println("Speedwire timed out"));
+        speedwire.onTimeout(() -> System.err.println("speedwire timeout"));
         speedwire.onData(data -> {
             if (data instanceof EnergyMeterTelegram) {
-                EnergyMeterTelegram energyMeterData = (EnergyMeterTelegram) data;
-                System.out.println("IP: " + energyMeterData.getOrigin().getHostAddress());
-                System.out.println("SUSy-ID: " + energyMeterData.getSUSyID());
-                System.out.println("Ser-No: " + energyMeterData.getSerNo());
-                System.out.println("Software: " + energyMeterData.getSoftwareVersion());
-                System.out.println("Time: " + energyMeterData.getMeasuringTime());
-                System.out.println(EnergyMeterChannels.TOTAL_P_IN + " : "
-                        + energyMeterData.getData(EnergyMeterChannels.TOTAL_P_IN).to(Units.WATT));
-                System.out.println(EnergyMeterChannels.TOTAL_P_IN_SUM + " : "
-                        + energyMeterData.getData(EnergyMeterChannels.TOTAL_P_IN_SUM)
-                        .to(MetricPrefix.KILO(Units.WATT).multiply(Units.HOUR).asType(Energy.class)));
+                EnergyMeterTelegram em = (EnergyMeterTelegram) data;
+
+                //device information
+                int SUSyID = em.getSUSyID();
+                long SerNo = em.getSerNo().longValueExact();
+                String ip = em.getOrigin().getHostAddress();
+                System.out.printf("Device %d %d on port %s%n", SUSyID, SerNo, ip);
+
+                //current power draw (in W)
+                Quantity<Power> w = em.getData(EnergyMeterChannels.TOTAL_P_IN).to(Units.WATT);
+                System.out.printf("Ingress Power: %s%n", w);
+
+                //energy meter total power reading (in kWh)
+                Quantity<Energy> powerReading = em.getData(EnergyMeterChannels.TOTAL_P_IN_SUM)
+                        .to(MetricPrefix.KILO(Units.WATT).multiply(Units.HOUR).asType(Energy.class));
+                System.out.printf("Total power reading: %s%n", powerReading);
             }
         });
         speedwire.start();
